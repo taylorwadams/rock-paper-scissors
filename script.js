4d454606e95f14c1e550411fe59400f571ef347e
Taylor <taylorwadams@gmail.com>
2021-05-28 20:03:44 -0400

    finished outcomes for all choices and added scoreboard functionality

--- script.js
+++ script.js
@@ -36,12 +36,104 @@ function playRound(playerChoice, compChoice) {
         return outcome.innerHTML = "You both chose " + playerChoice + ", it's a tie!";
 
     //outcomes for rock
-    } else if (playerChoice == "Rock" && compChoice == "Scissors" || "Lizard") {
+    } else if (playerChoice == "Rock" && compChoice == "Scissors") {
+        addPlayerScore()
+        return outcome.innerHTML = playerChoice + " crushes " + compChoice + ", YOU WIN!";
+    } else if (playerChoice == "Rock" && compChoice == "Lizard") {
+        addPlayerScore()
         return outcome.innerHTML = playerChoice + " crushes " + compChoice + ", YOU WIN!";
     } else if (playerChoice == "Rock" && compChoice == "Spock") {
+        addCompScore()
         return outcome.innerHTML = playerChoice + " is vaporized by " + compChoice + ", you lose.";
     } else if (playerChoice == "Rock" && compChoice == "Paper") {
+        addCompScore()
         return outcome.innerHTML = playerChoice + " is covered by " + compChoice + ", you lose.";
+    }
+
+    //outcomes for paper
+    else if(playerChoice == "Paper" && compChoice == "Rock") {
+        addPlayerScore()
+        return outcome.innerHTML = playerChoice + " covers " + compChoice + ", YOU WIN!";
+    } else if(playerChoice == "Paper" && compChoice == "Spock") {
+        addPlayerScore()
+        return outcome.innerHTML = playerChoice + " disproves " + compChoice + ", YOU WIN!";
+    } else if(playerChoice == "Paper" && compChoice == "Scissors") {
+        addCompScore()
+        return outcome.innerHTML = playerChoice + " is cut by " + compChoice + ", you lose.";
+    } else if(playerChoice == "Paper" && compChoice == "Lizard") {
+        addCompScore()
+        return outcome.innerHTML = playerChoice + " is eaten by " + compChoice + ", you lose.";
+    }
+
+    //outcomes for scissors
+    else if(playerChoice == "Scissors" && compChoice == "Lizard") {
+        addPlayerScore()
+        return outcome.innerHTML = playerChoice + " decapitates " + compChoice + ", YOU WIN!";
+    } else if(playerChoice == "Scissors" && compChoice == "Paper") {
+        addPlayerScore()
+        return outcome.innerHTML = playerChoice + " cuts " + compChoice + ", YOU WIN!";
+    } else if(playerChoice == "Scissors" && compChoice == "Rock") {
+        addCompScore()
+        return outcome.innerHTML = playerChoice + " is crushed by " + compChoice + ", you lose.";
+    } else if(playerChoice == "Scissors" && compChoice == "Spock") {
+        addCompScore()
+        return outcome.innerHTML = playerChoice + " is smashed by " + compChoice + ", you lose.";
+    }
+
+    //outcomes for Spock
+    else if(playerChoice == "Spock" && compChoice == "Scissors") {
+        addPlayerScore()
+        return outcome.innerHTML = playerChoice + " smashes " + compChoice + ", YOU WIN!";
+    } else if(playerChoice == "Spock" && compChoice == "Rock") {
+        addPlayerScore()
+        return outcome.innerHTML = playerChoice + " vaporizes " + compChoice + ", YOU WIN!";
+    } else if(playerChoice == "Spock" && compChoice == "Lizard") {
+        addCompScore()
+        return outcome.innerHTML = playerChoice + " is poisoned by " + compChoice + ", you lose.";
+    } else if(playerChoice == "Spock" && compChoice == "Paper") {
+        addCompScore()
+        return outcome.innerHTML = playerChoice + " is disproved by " + compChoice + ", you lose.";
+    }
+
+    //outcomes for Lizard
+    else if(playerChoice == "Lizard" && compChoice == "Spock") {
+        addPlayerScore()
+        return outcome.innerHTML = playerChoice + " poisons " + compChoice + ", YOU WIN!";
+    } else if(playerChoice == "Spock" && compChoice == "Paper") {
+        addPlayerScore()
+        return outcome.innerHTML = playerChoice + " eats " + compChoice + ", YOU WIN!";
+    } else if(playerChoice == "Spock" && compChoice == "Rock") {
+        addCompScore()
+        return outcome.innerHTML = playerChoice + " is crushed by " + compChoice + ", you lose.";
+    } else if(playerChoice == "Spock" && compChoice == "Scissors") {
+        addCompScore()
+        return outcome.innerHTML = playerChoice + " is decapitaed by " + compChoice + ", you lose.";
     }
 };
 
+
+//scoreboard functions
+
+function addPlayerScore() {
+    playerScore++;
+    document.getElementById("player-score").innerHTML = playerScore;
+    if (playerScore == 5) {
+        playerScore = 0;
+        document.getElementById("player-score").innerHTML = playerScore;
+        compScore = 0;
+        document.getElementById("comp-score").innerHTML = compScore;
+        return alert("You are the first to 5, You WIN!")
+    }
+};
+
+function addCompScore() {
+    compScore++;
+    document.getElementById("comp-score").innerHTML = compScore;
+    if (compScore == 5) {
+        playerScore = 0;
+        document.getElementById("player-score").innerHTML = playerScore;
+        compScore = 0;
+        document.getElementById("comp-score").innerHTML = compScore;
+        return alert("Computer is first to 5, You WIN!")
+    }
+};
